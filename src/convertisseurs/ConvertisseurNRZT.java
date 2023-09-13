b50ffb492ad98ac2d10612c9a3bd20a21cd611be
Morgan <morgan.couvrat@imt-atlantique.net>
2023-09-13 21:27:20 +0200

    Update ConvertisseurNRZT.java

--- src/convertisseurs/ConvertisseurNRZT.java
+++ src/convertisseurs/ConvertisseurNRZT.java
@@ -31,18 +31,54 @@ public class ConvertisseurNRZT <R,E> extends Convertisseur<Boolean,Float>{
     public void recevoir(Information<Boolean> information) throws InformationNonConformeException {
         this.informationRecue = information;
         this.informationEmise = new Information<Float>();
-        for (int i = 0; i < information.nbElements(); i++) {
-            if (information.iemeElement(i)) {
+
+
+
+
+        //on integre l'information emise
+        float integrale = 0;
+        for (int i = 0; i < informationEmise.nbElements(); i++) {
+            integrale += informationEmise.iemeElement(i);
+            informationEmise.setIemeElement(i, integrale);
+        }
+        
+        for (int i = 0; i < informationRecue.nbElements(); i++) {
+            if (informationRecue.iemeElement(i)) {
                 for (int j = 0; j < nbEchantillons; j++) {
-                    informationEmise.add(vMax);
+                    if (j < nbEchantillons / 3) {
+                        if (i != 0 && informationRecue.iemeElement(i - 1))
+                            informationEmise.add(vMax);
+                        else
+                            informationEmise.add((float) j / (nbEchantillons / 3) * vMax);
+                    } else if (j < 2 * nbEchantillons / 3) {
+                        informationEmise.add(vMax);
+                    } else {
+                        if (i + 1 < informationRecue.nbElements() && informationRecue.iemeElement(i + 1))
+                            informationEmise.add(vMax);
+                        else
+                            informationEmise.add((float) (nbEchantillons - j) / (nbEchantillons / 3) * vMax);
+                    }
                 }
             } else {
                 for (int j = 0; j < nbEchantillons; j++) {
-                    informationEmise.add(vMin);
+                    if (j < nbEchantillons / 3) {
+                        if (i != 0 && !informationRecue.iemeElement(i - 1))
+                            informationEmise.add(vMin);
+                        else
+                            informationEmise.add((float) j / (nbEchantillons / 3) * vMin);
+                    } else if (j < 2 * nbEchantillons / 3) {
+                        informationEmise.add(vMin);
+                    } else {
+                        if (i + 1 < informationRecue.nbElements() && !informationRecue.iemeElement(i + 1))
+                            informationEmise.add(vMin);
+                        else
+                            informationEmise.add((float) (nbEchantillons - j) / (nbEchantillons / 3) * vMin);
+                    }
                 }
             }
+
         }
-         
+
         this.emettre();
     }
 
