a694726ea94cd6edc055217cf402404ec03fdf5d
Morgan <morgan.couvrat@imt-atlantique.net>
2023-09-21 17:15:01 +0200

    Supression des affichages intempestifs

--- src/transmetteurs/TransmetteurBruiteAnalogique.java
+++ src/transmetteurs/TransmetteurBruiteAnalogique.java
@@ -45,13 +45,13 @@ public class TransmetteurBruiteAnalogique extends Transmetteur <Float,Float> {
 		}
 		puissance_signal=puissance_signal/information.nbElements();
 
-		System.out.println("Puissance du signal : "+puissance_signal);
+		//System.out.println("Puissance du signal : "+puissance_signal);
 
 		//calcul de la puissance du bruit
 		float puissance_bruit = puissance_signal/SNRPB;
 		
 		//affichage du rapport signal sur bruit
-		System.out.println("Rapport signal sur bruit : "+10*Math.log10(puissance_signal/puissance_bruit)+" dB");
+		//System.out.println("Rapport signal sur bruit : "+10*Math.log10(puissance_signal/puissance_bruit)+" dB");
 
 		//ajout du bruit au signal
 		double variance = (float) (Math.sqrt((nbEchantillons*puissance_signal)/(2*SNRPB)));
